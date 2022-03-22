08cd5117d89c947caa91715259b8c40325d19684
Faheem-Akhund <90100547+Faheem-Akhund@users.noreply.github.com>
2022-03-22 18:31:09 +0500

    methods added in repo 2

--- src/main/java/io/javabrains/springsecurityjpa/Cart/CartRepository.java
+++ src/main/java/io/javabrains/springsecurityjpa/Cart/CartRepository.java
@@ -1,6 +1,15 @@
 package io.javabrains.springsecurityjpa.Cart;
 
+import io.javabrains.springsecurityjpa.User.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface CartRepository extends JpaRepository<Cart, Integer> {
+
+    Optional<List<Cart>> findByUserAndStatus(User user, String status);
+    Optional<List<Cart>> findByUserAndStatusNotAndStatusNot(User user,String approved,String pending);
+    Optional<Cart>findByIdAndUserAndStatus(Integer id,User user,String status);
+
 }
