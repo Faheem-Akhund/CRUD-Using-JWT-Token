e3f884f0bf469555ac4937c43432b778aaecd5f6
Faheem-Akhund <90100547+Faheem-Akhund@users.noreply.github.com>
2022-03-24 15:22:52 +0500

    added 2 jpa-querries !

--- src/main/java/io/javabrains/springsecurityjpa/Product/ProductRepository.java
+++ src/main/java/io/javabrains/springsecurityjpa/Product/ProductRepository.java
@@ -1,11 +1,26 @@
 package io.javabrains.springsecurityjpa.Product;
 
+import io.lettuce.core.dynamic.annotation.Param;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+
+    @Query(value = "SELECT u FROM Product u WHERE u.category=:name")
+    List<Product> findAllProducts(Sort sort,String name);
+
+    @Query(value = "SELECT u FROM Product u WHERE u.category=:name")
+    List<Product> findProductsbycategory(Sort sort,String name);
+
 
 
 
