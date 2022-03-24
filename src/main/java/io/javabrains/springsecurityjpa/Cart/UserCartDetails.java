17a21d41faea09e87c378206ddb851cedd893a0b
Faheem-Akhund <90100547+Faheem-Akhund@users.noreply.github.com>
2022-03-24 11:49:39 +0500

    list type cart was changed from cart to cartdto

--- src/main/java/io/javabrains/springsecurityjpa/Cart/UserCartDetails.java
+++ src/main/java/io/javabrains/springsecurityjpa/Cart/UserCartDetails.java
@@ -3,6 +3,7 @@ package io.javabrains.springsecurityjpa.Cart;
 import io.javabrains.springsecurityjpa.Cart.Cart;
 import lombok.Getter;
 import lombok.Setter;
+import org.omg.CORBA.Object;
 
 import java.util.List;
 import java.util.Set;
@@ -14,7 +15,7 @@ public class UserCartDetails {
 
     private int id;
     private String userName;
-    private List<Cart> cart;
-
+    private List<CartDTO> cart;
+//    private Object cart;
 
 }
