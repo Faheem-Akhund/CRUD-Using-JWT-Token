21e4f157f3b0b25a8d341105bcb8c0c245b06089
Faheem-Akhund <90100547+Faheem-Akhund@users.noreply.github.com>
2022-03-24 15:24:11 +0500

    added category type string

--- src/main/java/io/javabrains/springsecurityjpa/Product/Product.java
+++ src/main/java/io/javabrains/springsecurityjpa/Product/Product.java
@@ -19,6 +19,7 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    private String category;
     private String productName;
     private Integer price;
 
